7abcf6f957a0a412b99a331da7818dd62e9babfd
Reilly Miller <reillyjmiller31@gmail.com>
2023-03-21 21:51:14 -0600

    create artist, add relationship between song and artist

---
+++ spec/features/songs/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'the songs show page' do
+  it 'displays the song title' do
+    artist = Artist.create!(name: 'Carly Rae Jepsen')
+    # song = Song.create!(title: "I Really Like You", length: 208, play_count: 2456357)
+    song = artist.songs.create!(title: "I Really Like You", length: 208, play_count: 2456357)
+    song_2 = artist.songs.create!(title: "Call Me Maybe", length: 202, play_count: 21456357)
+
+    visit "/songs/#{song.id}"
+    expect(page).to have_content(song.title)
+    expect(page).to_not have_content(song_2.title)
+  end
+
+  it 'displays the name of the artist for the song' do
+    artist = Artist.create!(name: 'Carly Rae Jepsen')
+    # song = Song.create!(title: "I Really Like You", length: 208, play_count: 2456357)
+    song = artist.songs.create!(title: "I Really Like You", length: 208, play_count: 2456357)
+    song_2 = artist.songs.create!(title: "Call Me Maybe", length: 202, play_count: 21456357)
+
+    visit "/songs/#{song.id}"
+    save_and_open_page
+    expect(page).to have_content(artist.name)
+  end
+end
